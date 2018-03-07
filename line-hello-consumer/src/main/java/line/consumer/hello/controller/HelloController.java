f80c15e2f74465557a28bc22fc5df612deacd1d7
linewon <linewon1983@gmail.com>
2018-03-07 10:58:51 +0800

    添加日志系统：slf4j + logback + lombok

--- line-hello-consumer/src/main/java/line/consumer/hello/controller/HelloController.java
+++ line-hello-consumer/src/main/java/line/consumer/hello/controller/HelloController.java
@@ -6,8 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import line.consumer.hello.feign.client.HelloServiceFeignClient;
+import lombok.extern.slf4j.Slf4j;
 
 @Controller
+@Slf4j
 public class HelloController {
 
 	@Autowired
@@ -16,6 +18,8 @@ public class HelloController {
 	@GetMapping("/hello-from-other-side")
 	@ResponseBody
 	public String helloFromHelloService() {
+		// 使用 slf4j + logback + lombok 的日志系统
+		log.info("this is the LOG_TEXT!");
 		try {
 			String resp = helloServiceFeignClient.hello();
 			return resp;
